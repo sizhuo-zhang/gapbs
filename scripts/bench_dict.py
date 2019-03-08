d3b46fdcabc8aba89499eec5cc5f0b7ff8d19df2
Sizhuo Zhang <szzhang@mit.edu>
2019-03-07 19:22:10 -0500

    better linux generation

--- scripts/bench_dict.py
+++ scripts/bench_dict.py
@@ -1,16 +1,34 @@
 # graph -> benchmark -> args
-bench_dict = {
+arg_dict = {
     'roadU.sg': {
-        'tc': '',
+        'tc' : '',
     },
     'road.sg': {
         'bfs': '',
-        'pr': '-i1000 -t1e-4',
-        'cc': '',
-        'bc': '-i4',
+        'pr' : '-i1000 -t1e-4',
+        'cc' : '',
+        'bc' : '-i4',
     },
     'road.wsg': {
         'sssp': '-d50000',
     },
 }
 
+run_dict = {
+    'test': {
+        'tc'  : 1,
+        'bfs' : 1,
+        'pr'  : 1,
+        'cc'  : 1,
+        'bc'  : 1,
+        'sssp': 1,
+    },
+    'ref': {
+        'tc'  : 3,
+        'bfs' : 64,
+        'pr'  : 16,
+        'cc'  : 16,
+        'bc'  : 16,
+        'sssp': 64,
+    },
+}
--- scripts/gen_linux_image.py
+++ scripts/gen_linux_image.py
@@ -4,11 +4,12 @@ import os
 import shutil
 import subprocess
 import argparse
-from bench_dict import bench_dict
+from bench_dict import arg_dict, run_dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--outdir', dest = 'out_dir', required = True)
 parser.add_argument('--jobs', dest = 'jobs', required = False, default = 1)
+parser.add_argument('--depld', dest = 'dep_ld_fence', action = 'store_true')
 args = parser.parse_args()
 
 # riscy dirs
@@ -24,7 +25,7 @@ bin_dir = '../build/riscv'
 out_dir = os.path.abspath(args.out_dir)
 test_dir = os.path.join(out_dir, 'test') # build initramfs for this benchmark
 
-for graph, bench_params in bench_dict.iteritems():
+for graph, bench_params in arg_dict.iteritems():
     print ''
     print '========================================='
     print 'Generating gapbs graph {} ...'.format(graph)
@@ -56,24 +57,29 @@ for graph, bench_params in bench_dict.iteritems():
             continue
         shutil.copy(binary, test_dir)
 
-        # write run.sh
-        run_sh = os.path.join(test_dir, bench + '.sh')
-        if os.path.isfile(run_sh):
-            raise Exception('run.sh already exists!')
-        with open(run_sh, 'w') as fp:
-            fp.write('#!/bin/ash\n')
-            fp.write('if [ $# -ne 1 ]; then\n')
-            fp.write('  echo "Usage: ./run.sh THREAD_NUM"\n')
-            fp.write('  exit\n')
-            fp.write('fi\n')
-            fp.write('export OMP_NUM_THREADS=$1\n')
-            fp.write('time ./{} -f {} -a -n1 {}\n'.format(bench, graph, param))
-        os.chmod(run_sh, int('0777', 8))
+        # write run.sh, test and ref
+        for size in ['test', 'ref']:
+            run_sh = os.path.join(test_dir, bench + '-' + size + '.sh')
+            if os.path.isfile(run_sh):
+                raise Exception(run_sh + ' already exists!')
+            with open(run_sh, 'w') as fp:
+                fp.write('#!/bin/ash\n')
+                fp.write('if [ $# -ne 1 ]; then\n')
+                fp.write('  echo "Usage: $0 ./ THREAD_NUM"\n')
+                fp.write('  exit\n')
+                fp.write('fi\n')
+                fp.write('export OMP_NUM_THREADS=$1\n')
+                fp.write('time ./{} -f {} -a -n{} {}\n'.format(
+                         bench, graph, run_dict[size][bench], param))
+            os.chmod(run_sh, int('0777', 8))
 
     # compile and copy linux (bbl)
-    subprocess.check_call([build_linux_script,
-                           '--jobs', str(args.jobs),
-                           '--testdir', test_dir])
+    cmd = [build_linux_script,
+           '--jobs', str(args.jobs),
+           '--testdir', test_dir]
+    if args.dep_ld_fence:
+        cmd.append('--depld')
+    subprocess.check_call(cmd)
     shutil.copy(bbl_path,
                 os.path.join(out_dir,
                              '_'.join(['bbl', 'gapbs', graph])))
